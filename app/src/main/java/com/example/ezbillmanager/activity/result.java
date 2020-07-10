ef95469373d3299e56b464e711c6c436246edcde
louroborus <1908420450@qq.com>
2020-07-10 22:25:59 +0800

    更新

--- app/src/main/java/com/example/ezbillmanager/activity/result.java
+++ app/src/main/java/com/example/ezbillmanager/activity/result.java
@@ -2,6 +2,7 @@ package com.example.ezbillmanager.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,8 +25,10 @@ public class result extends AppCompatActivity {
         date2.findViewById(R.id.result_date2);
         btn=findViewById(R.id.result_btn_return);
         //获取从query获取的日期，格式为：年.月.日
-        String Date1 =(String)getIntent().getCharSequenceExtra("startdate");
-        String Date2 =(String)getIntent().getCharSequenceExtra("enddate");
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String Date1 =bundle.getString("startdate");
+        String Date2 =bundle.getString("enddate");
         //设置日期文本显示
         date1.setText(Date1);
         date2.setText(Date2);
